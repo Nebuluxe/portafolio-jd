cd73085f77453f553bc2e82c3a3d6f9e9ba67105
eljavexe <jadiazs2003@gmail.com>
2024-05-29 18:34:50 -0400

    Nuevas mejoras al front

--- js/site.js
+++ js/site.js
@@ -1,13 +1,4 @@
-// document.addEventListener("DOMContentLoaded", function() {
-//     const track = document.querySelector('.skills-track');
-//     const skills = Array.from(track.children);
 
-//     // Clonar los elementos para crear el efecto infinito
-//     skills.forEach(skill => {
-//         const clone = skill.cloneNode(true);
-//         track.appendChild(clone);
-//     });
-// });
 
 document.addEventListener('DOMContentLoaded', function() {
     const track = document.querySelector('.carousel-track');
