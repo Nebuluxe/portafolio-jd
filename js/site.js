e292e137683fb03426555389586998a321a0a36e
Joaquin Diaz <jadiazs2003@gmail.com>
2024-10-28 19:41:46 -0300

    Se integran mejoras en html y css

--- js/site.js
+++ js/site.js
@@ -1,76 +1,18 @@
+document.getElementById('toggleLightButton').addEventListener('click', function() {
+    const light = document.querySelector('.light');
+    const lightIcon = document.getElementById('lightIcon');
+    const buttonText = document.getElementById('buttonText');
+    const button = document.getElementById('toggleLightButton');
 
+    // Cambiar la opacidad de la luz
+    light.style.opacity = light.style.opacity === '0' ? '0.8' : '0';
 
-document.addEventListener('DOMContentLoaded', function() {
-    const track = document.querySelector('.carousel-track');
-    const items = Array.from(track.children);
-    const itemWidth = items[0].getBoundingClientRect().width;
-    let currentIndex = 0;
-
-    function moveToNextItem() {
-        items.forEach((item, index) => {
-            item.classList.remove('active');
-        });
-        currentIndex = (currentIndex + 1) % items.length;
-        items[currentIndex].classList.add('active');
-        const moveAmount = -currentIndex * itemWidth;
-        track.style.transform = `translateX(${moveAmount}px)`;
-    }
-
-    // Inicializa el primer elemento como activo
-    items[0].classList.add('active');
-
-    // Mueve al siguiente elemento cada 2 segundos
-    setInterval(moveToNextItem, 2000);
-});
-
-document.addEventListener('DOMContentLoaded', function() {
-    const links = document.querySelectorAll('#navbar a');
-
-    for (const link of links) {
-        link.addEventListener('click', function(event) {
-            if (this.hash !== "") {
-                event.preventDefault();
-                const hash = this.hash;
-                smoothScrollTo(document.querySelector(hash), 1000); // 1000 ms = 1 segundo
-            }
-        });
+    // Cambiar el icono y el texto del botón
+    if (light.style.opacity === '0') {
+        lightIcon.classList.replace('bi-lightbulb', 'bi-lightbulb-off');
+        button.classList.remove('light-on');
+    } else {
+        lightIcon.classList.replace('bi-lightbulb-off', 'bi-lightbulb');
+        button.classList.add('light-on');
     }
 });
-
-document.addEventListener('DOMContentLoaded', function() {
-    const links = document.querySelectorAll('#showcase a');
-
-    for (const link of links) {
-        link.addEventListener('click', function(event) {
-            if (this.hash !== "") {
-                event.preventDefault();
-                const hash = this.hash;
-                smoothScrollTo(document.querySelector(hash), 1000); // 1000 ms = 1 segundo
-            }
-        });
-    }
-});
-
-function smoothScrollTo(target, duration) {
-    const targetPosition = target.getBoundingClientRect().top;
-    const startPosition = window.pageYOffset;
-    const distance = targetPosition;
-    let startTime = null;
-
-    function animation(currentTime) {
-        if (startTime === null) startTime = currentTime;
-        const timeElapsed = currentTime - startTime;
-        const run = ease(timeElapsed, startPosition, distance, duration);
-        window.scrollTo(0, run);
-        if (timeElapsed < duration) requestAnimationFrame(animation);
-    }
-
-    function ease(t, b, c, d) {
-        t /= d / 2;
-        if (t < 1) return c / 2 * t * t + b;
-        t--;
-        return -c / 2 * (t * (t - 2) - 1) + b;
-    }
-
-    requestAnimationFrame(animation);
-}
