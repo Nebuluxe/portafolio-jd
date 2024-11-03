c85d918d3e0c15f982f241ecd69390b8ed1a4de5
eljavexe <jadiazs2003@gmail.com>
2024-11-03 16:45:32 -0300

    mejoras generales al aspecto

--- js/site.js
+++ js/site.js
@@ -1,4 +1,5 @@
 document.getElementById('toggleLightButton').addEventListener('click', function() {
+    const lamp = document.querySelector('.lamp');
     const light = document.querySelector('.light');
     const lightIcon = document.getElementById('lightIcon');
     const button = document.getElementById('toggleLightButton');
@@ -11,11 +12,15 @@ document.getElementById('toggleLightButton').addEventListener('click', function(
     if (light.style.opacity === '0') {
         lightIcon.classList.replace('bi-lightbulb', 'bi-lightbulb-off');
         button.classList.remove('light-on');
-        body.style.background = '#0f1018'; // Fondo cuando la luz está apagada
+        body.style.background = '#0f1018'; 
+        lamp.style.boxShadow = 'none';
+        lamp.style.animation = 'none';
     } else {
         lightIcon.classList.replace('bi-lightbulb-off', 'bi-lightbulb');
         button.classList.add('light-on');
         body.style.background = 'linear-gradient(to bottom, #1a1c28 0%, #12131c 50%, #0b0c11 100%)'; // Fondo cuando la luz está encendida
+        lamp.style.boxShadow = '0px 0px 10px rgba(255, 255, 0, 0.8)';
+        lamp.style.animation = 'swing 2s infinite ease-in-out';
     }
 });
 
