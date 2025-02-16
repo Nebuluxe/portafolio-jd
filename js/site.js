06b9618505ed382dce47f306d55901a5f4b1ad09
Joaquin Diaz <jadiazs2003@gmail.com>
2025-02-16 14:26:05 -0300

    feat: Mejoras en diseño y actualización de CV
    - Se actualizaron los estilos de la sección "Acerca de mí" en index.html.
    - Se eliminaron imágenes de prueba de la carpeta img.
    - Se actualizó el archivo Joaquin-Diaz-CV.pdf con información reciente.
    - Se realizaron ajustes menores en el script site.js para mejorar la funcionalidad del formulario de contacto.

--- js/site.js
+++ js/site.js
@@ -32,11 +32,3 @@ document.getElementById("toggleContactForm").addEventListener("click", function 
         contactForm.style.display = "none";
     }
 });
-document.getElementById("moreAbout").addEventListener("click", function () {
-    var contactForm = document.getElementById("more-information");
-    if (contactForm.style.display === "none" || contactForm.style.display === "") {
-        contactForm.style.display = "block";
-    } else {
-        contactForm.style.display = "none";
-    }
-});
